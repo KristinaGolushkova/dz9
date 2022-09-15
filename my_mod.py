8245cbf05e73439b5ffadf62098e463ccc852b3a
KristinaGolushkova <107718947+KristinaGolushkova@users.noreply.github.com>
2022-09-16 00:52:46 +0300

    one

---
+++ my_mod.py
@@ -0,0 +1,18 @@
+def count_lines(name=input('enter name file \n')):
+    file = open(name, 'r')
+    count = file.readlines()
+    print(len(count))
+
+
+def count_chars(name=input('enter name file \n')):
+    file = open(name, 'r')
+    count = file.read()
+    print(len(count))
+
+
+def test():
+    return count_chars(), count_lines()
+
+test()
+
+
